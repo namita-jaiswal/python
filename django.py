f3c2a342a2d377044727f6525f1fb45a95fd993a
Namita Jaiswal <namitajaiswal826@gmail.com>
2025-10-03 19:49:50 +0530

    django day 2

--- django.py
+++ django.py
@@ -5,3 +5,5 @@ chai("ginger tea")
 chaiOne="masala chai"
 chaaiTwo="lemon tea"
 chaiThree="iced tea"
+print(r"c:\\user\\pwd\\newfolder") # raw string
+print("My favorite tea is %s" % chaiOne)
---
+++ scope.py
@@ -0,0 +1,4 @@
+username="chaiaurcode"
+
+def func():
+    username = "chaiaurcode"
